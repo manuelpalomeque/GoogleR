e7a4b9510e89c65dbd9e9bcbad08e63330dad0f2
manuelpalomeque <pjonathanmanuel@hotmail.com>
2024-04-13 17:01:39 -0300

    introduccion a tidyverse

---
+++ 07 - Tidyverse.R
@@ -0,0 +1,14 @@
+# Cargar tidyverse;
+
+library(tidyverse)
+# Paquetes que se instalan ggplot2 , tibble, tidyr, readr, purr, dplyr
+# stringr y forcats
+
+# Para buscar actualizaciones de tidyverse:
+tidyverse_update()
+
+# actualizar todos los paquetes:
+update.packages()
+
+# leer la documentacion oficial del paquete cargado:
+browseVignettes("tidyverse")
