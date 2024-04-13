a26f27f031906ba0c760c09d342bbda424703d5c
manuelpalomeque <pjonathanmanuel@hotmail.com>
2024-04-13 16:23:53 -0300

    Clase 01

---
+++ 01- Variables, vectores y canalizaciones.R
@@ -0,0 +1,14 @@
+# Esta es mi primer variable en R:
+
+primer_variable <- "Tu puedes"
+segunda_variable <- 12.5
+
+# mi primer vector;
+vector_1 <- c(13, 83.5, 102, 68)
+
+#canalizaciones:
+
+ToothGrowth %>% filter(dose==0.50) %>% arrange(len)
+
+str(list("a", 1L, 1.5, TRUE))
+
