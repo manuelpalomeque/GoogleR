151e9f42016e14ac034d99c32c1d22045ecf67b8
manuelpalomeque <pjonathanmanuel@hotmail.com>
2024-04-13 16:34:27 -0300

    Calculos

---
+++ 04 - Operaciones y calculos.R
@@ -0,0 +1,14 @@
+ventas_1 <- 35976.98
+ventas_2 <- 74645
+
+# suma:
+ventas_anuales <- ventas_1 + ventas_2
+
+#resta:
+resta <- ventas_1 - ventas_2
+
+# multiplicacion:
+multiplicacion <- ventas_anuales * 2
+
+# division:
+division <- ventas_anuales / ventas_1
