82671a98cba66a22a0eb2f0c9a915a75db5b4991
manuelpalomeque <pjonathanmanuel@hotmail.com>
2024-04-13 16:37:39 -0300

    Condicionales

---
+++ 06 - Condicionales.R
@@ -0,0 +1,20 @@
+#IF
+x <- 4
+
+if (x >0) {
+  print("La variable x es un numero positivo")
+} else {
+  print(" La variable x es un numero negativo")
+}
+
+
+# ElseIf:
+y <- -1
+
+if (y < 0) {
+  print("y es un número negativo")
+} else if (y == 0) {
+  print("y es cero")
+} else {
+  print("y es un número positivo")
+}
